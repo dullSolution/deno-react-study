ceaef8322b6f7569ad63457a18fd3c69d27f6f8c
lochyb <lochy711@gmail.com>
2025-03-20 15:27:10 +0900

    Tired to get inference typing to work but is rather annoying

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import deno from "@deno/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 import "react";
 import "react-dom";
@@ -9,9 +10,16 @@ export default defineConfig({
   root: "./client",
   server: {
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
   },
   plugins: [
     react(),
+    tailwindcss(),
     deno(),
   ],
   optimizeDeps: {
